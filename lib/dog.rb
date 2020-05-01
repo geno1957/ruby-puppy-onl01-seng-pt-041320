bf9d00a0985bbb4d5bcaed0fb3c8a4ea7466c971
Jared  E <jared.epps322@gmail.com>
2020-05-01 21:54:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,14 +14,14 @@ class Dog
     @@all.each do |dog|
       puts dog.name
     end
-  end 
+  
   
 
 def self.print_all
   @@all.each do |dog|
   puts dog.name 
 end 
-end 
+
 
 
   def self.clear_all
